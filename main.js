8cbea076c367ecd5799030f658316b48e738b43f
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-17 23:09:16 +0530

    ajax

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function addRecord() {
-  let inputMessageRef = document.querySelector("messageId");
+  let inputMessageRef = document.querySelector("#messageId");
   let message = inputMessageRef.value;
   let uri = `http://localhost:4000/addRecord?message=${message}`;
   fetch(uri);
