e20ac0d1283b0169f9699697d779435811237a89
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-17 23:40:33 +0530

    Front-end-Integration

--- main.js
+++ main.js
@@ -3,4 +3,21 @@ function addRecord() {
   let message = inputMessageRef.value;
   let uri = `http://localhost:4000/addRecord?message=${message}`;
   fetch(uri);
+  inputMessageRef.value = "";
+  findAllMessage();
+}
+
+async function findAllMessage() {
+  let uri = "http://localhost:4000/findAll";
+  let res = await fetch(uri);
+  let list = await res.json();
+  console.log(list);
+
+  for (let item of list) {
+    let parentRef = document.querySelector("#parent");
+    let existingValue = parentRef.innerHTML;
+    let newValue = `<h1>${item.message}</h1>`;
+    parentRef.innerHTML = newValue + existingValue;
+    console.log(item);
+  }
 }
